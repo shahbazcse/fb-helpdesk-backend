9c92c45aaaef64df4f64adb135a9321c479fc299
shahbazcse <cseshahbaz@gmail.com>
2024-02-09 18:03:47 +0530

    Refactored Code

--- index.js
+++ index.js
@@ -220,27 +220,28 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-// webhook
-app.post("/webhook", (req, res) => {
+// Webhook
+app.post('/webhook', (req, res) => {
+
   let body = req.body;
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
-    // body.entry.forEach(function (entry) {
-    //   // Gets the body of the webhook event
-    //   let webhook_event = entry.messaging[0];
-    //   console.log(webhook_event);
 
-    //   // Get the sender PSID
-    //   let sender_psid = webhook_event.sender.id;
-    //   if (webhook_event.message) {
-    //     return 1;
-    //   }
-    // });
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+
 });
